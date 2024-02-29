e8fef5f5e098b2f0b27f05c9ed317ce67f4a41a2
BryaN <60313619+BryanLTU@users.noreply.github.com>
2024-02-29 22:51:32 +0200

    update: Button design and Background
    update: Button design and title screen background

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -8,4 +8,8 @@ public class MainMenu : MonoBehaviour
     public void LoadScene(string sceneName) {
         SceneManager.LoadSceneAsync(sceneName);
     }
+
+    public void QuitGame() {
+        Application.Quit();
+    }
 }

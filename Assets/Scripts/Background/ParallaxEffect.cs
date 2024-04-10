739518bec6f2c95fcb04a1ca3a7d3e9492831693
BryaN <60313619+BryanLTU@users.noreply.github.com>
2024-04-10 22:19:25 +0300

    feature: Parallax Effect background Unit Tests

--- Assets/Scripts/Background/ParallaxEffect.cs
+++ Assets/Scripts/Background/ParallaxEffect.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class ParallaxEffect : MonoBehaviour
@@ -13,14 +14,29 @@ public class ParallaxEffect : MonoBehaviour
     }
 
     void LateUpdate() {
+        UpdateParallax();
+    }
+
+    public void UpdateParallax() {
         Vector3 cameraMovement = _camera.transform.position - cameraPos;
         transform.position += cameraMovement * parallaxMultiplier;
         cameraPos = _camera.transform.position;
 
         if (Mathf.Abs(_camera.transform.position.x - transform.position.x) >= repeatOffset) {
-            float offsetX = (_camera.transform.position - transform.position).x % repeatOffset;
+            float offsetX = 0f;
+
+            if (repeatOffset > 0f)
+                offsetX =  (_camera.transform.position - transform.position).x % repeatOffset;
 
             transform.position = new Vector3(_camera.transform.position.x + offsetX, transform.position.y, transform.position.z);
         }
     }
+
+    #if UNITY_INCLUDE_TESTS
+    
+        public void SetCameraGameObject(GameObject c) { _camera = c; }
+        public void SetParallaxMultiplier(float parallaxMultiplier) {  this.parallaxMultiplier = parallaxMultiplier; }
+        public void SetRepeatOffset(float repeatOffset) {  this.repeatOffset = repeatOffset; }
+
+    #endif
 }

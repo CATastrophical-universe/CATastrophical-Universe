8f71e0c3a400f77298b6b3dc8a8fd20b92a77358
BryaN <60313619+BryanLTU@users.noreply.github.com>
2024-04-17 14:26:41 +0300

    refactor: New scene and gameobjects creation logic

--- Assets/Tests/PlayMode/MovementTests.cs
+++ Assets/Tests/PlayMode/MovementTests.cs
@@ -7,34 +7,47 @@ using System.Collections;
 public class MovementTests
 {
 	private GameObject playerObject;
+    private GameObject groundObject;
 	private PlayerMovement playerMovement;
 	private Rigidbody2D rb;
-	private Transform groundCheck;
-	private LayerMask groundLayer;
+	private GameObject groundCheck;
 
 	[SetUp]
 	public void Setup()
 	{
-		playerObject = new GameObject("PlayerObject");
-		playerMovement = playerObject.AddComponent<PlayerMovement>();
-		rb = playerObject.AddComponent<Rigidbody2D>();
-		groundCheck = new GameObject().transform;
+        // Set up mocks if necessary (skipping complicated Unity specific stuff, focus on principle)
+		LayerMask groundLayer = 1 << 6;
+        Debug.Log(string.Format("{0} {1}", (int)groundLayer.value, LayerMask.NameToLayer("Ground")));
 
-		// Set up mocks if necessary (skipping complicated Unity specific stuff, focus on principle)
-		groundLayer = LayerMask.NameToLayer("Ground");
+        groundObject = new GameObject("Ground");
+        groundObject.AddComponent<BoxCollider2D>();
+        groundObject.transform.position -= new Vector3(0f, 10f, 0f);
+        groundObject.transform.localScale = new Vector2(10f, 1f);
+        groundObject.layer = groundLayer;
+
+		playerObject = new GameObject("PlayerObject");
+        playerObject.AddComponent<BoxCollider2D>();
+        rb = playerObject.AddComponent<Rigidbody2D>();
+        playerMovement = playerObject.AddComponent<PlayerMovement>();
+
+		groundCheck = new GameObject("GroundCheck");
+        groundCheck.transform.SetParent(playerObject.transform);
+        groundCheck.transform.position = new Vector3(0f, -0.6f, 0f);
 
 		// Use reflection or exposed functions to set up private fields if necessary
 		typeof(PlayerMovement).GetField("rb", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(playerMovement, rb);
-		typeof(PlayerMovement).GetField("groundCheck", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(playerMovement, groundCheck);
+		typeof(PlayerMovement).GetField("groundCheck", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(playerMovement, groundCheck.transform);
 		typeof(PlayerMovement).GetField("groundLayer", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(playerMovement, groundLayer);
+        // playerMovement.SetGroundLayer("Ground");
 	}
 
 	[UnityTest]
+    [Timeout(10000)]
 	public IEnumerator DoesPlayerMoveRight()
 	{
-		playerMovement.SetHorizontal(1f);
+        yield return new WaitWhile(() => !playerMovement.IsGrounded());
 
-		yield return new WaitForSeconds(1);
+		playerMovement.SetHorizontal(1f);
 
 		Debug.Log(playerObject.transform.position.x);
 
@@ -55,5 +68,7 @@ public class MovementTests
 	public void TearDown()
 	{
 		Object.DestroyImmediate(playerObject);
+        Object.DestroyImmediate(groundCheck);
+        Object.DestroyImmediate(groundObject);
 	}
 }

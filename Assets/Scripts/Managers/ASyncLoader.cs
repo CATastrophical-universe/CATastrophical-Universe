b39815ca58441797564aca7cc182a77b3e653149
BryaN <60313619+BryanLTU@users.noreply.github.com>
2024-04-21 12:41:02 +0300

    Loading screen (#15)
    * feature: Add scene loading script
    
    * chore: Add ASyncManager to the Main Menu scene

---
+++ Assets/Scripts/Managers/ASyncLoader.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ASyncLoader : MonoBehaviour
+{
+    [Header("Menu Screens")]
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject mainMenu;
+
+    [Header("Slider")]
+    [SerializeField] private Slider loadingSlider;
+
+    public void LoadLevelButton(string level)
+    {
+        mainMenu.SetActive(false);
+        loadingScreen.SetActive(true);
+
+        StartCoroutine(LoadLevelASync(level));
+    }
+
+    IEnumerator LoadLevelASync(string level)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(level);
+
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+
+            loadingSlider.value = progressValue;
+
+            yield return null;
+        }
+    }
+}

cac300ea003b489c5b6cb55b9721389b6bae9d5a
BryaN <60313619+BryanLTU@users.noreply.github.com>
2024-05-11 22:44:16 +0300

    refactor: Add boolean to check if the key should display on default

--- Assets/Scripts/Tutorial/Key.cs
+++ Assets/Scripts/Tutorial/Key.cs
@@ -8,18 +8,21 @@ public class Key
     [SerializeField] String keyName;
     [SerializeField] KeyCode keyCode;
     [SerializeField] String action;
+    [SerializeField] bool shouldNotDisplayOnDefault;
 
     public Sprite GetTexture() { return texture; }
     public String GetKeyName() { return keyName; }
     public KeyCode GetKeyCode() { return keyCode; }
     public String GetAction() { return action; }
+    public bool GetShouldNotDisplayOnDefault() { return shouldNotDisplayOnDefault; }
 
     #if UNITY_INCLUDE_TESTS
     
-        public Key(String keyName, KeyCode keyCode, String action) {
+        public Key(String keyName, KeyCode keyCode, String action, bool shouldNotDisplayOnDefault = false) {
             this.keyName = keyName;
             this.keyCode = keyCode;
             this.action = action;
+            this.shouldNotDisplayOnDefault = shouldNotDisplayOnDefault;
         }
 
     #endif

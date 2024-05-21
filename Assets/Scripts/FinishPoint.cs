c3a16ee36d5661cb642c008afb1365997da89576
Matas <162483840+ChernobylHub@users.noreply.github.com>
2024-05-21 22:24:00 +0300

    Update
    * Menu scripts moved to "menu" folder.
    * Updated FinishPoint script.
    * Added level LoadMenu.

--- Assets/Scripts/FinishPoint.cs
+++ Assets/Scripts/FinishPoint.cs
@@ -1,14 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement; 
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class FinishPoint : MonoBehaviour
 {
     [SerializeField] private string NextScene;
+
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Slider loadingSlider;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player")) 
         {
-            SceneManager.LoadSceneAsync(NextScene);
+            UnlockNewLevel();
+            LoadLevel(NextScene);
+        }
+    }
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public void LoadLevel(string level)
+    {
+        loadingScreen.SetActive(true);
+
+        StartCoroutine(LoadLevelASync(level));
+    }
+    IEnumerator LoadLevelASync(string level)
+    {
+        AsyncOperation loadOperation = SceneManager.LoadSceneAsync(level);
+
+        while (!loadOperation.isDone)
+        {
+            float progressValue = Mathf.Clamp01(loadOperation.progress / 0.9f);
+
+            loadingSlider.value = progressValue;
+
+            yield return null;
         }
     }
 }
